2aa54111bfe1b25012bdbe98d277692e1ed55dbf
phukhieo <akopadung>
2020-04-08 14:39:49 +0700

    Updates

---
+++ precache-manifest.af117344336f54ed5d1ee3efa1567c28.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44a40193ca854e7cdf4361f1af413f8e",
+    "url": "/test2/index.html"
+  },
+  {
+    "revision": "202bc9ac70a5df70044a",
+    "url": "/test2/static/css/2.3ab7218d.chunk.css"
+  },
+  {
+    "revision": "5d0e134b7cebd54b4655",
+    "url": "/test2/static/css/main.08a84b6d.chunk.css"
+  },
+  {
+    "revision": "202bc9ac70a5df70044a",
+    "url": "/test2/static/js/2.febf8e57.chunk.js"
+  },
+  {
+    "revision": "d1a764f5d3429e74a28c2434a12f9a08",
+    "url": "/test2/static/js/2.febf8e57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d0e134b7cebd54b4655",
+    "url": "/test2/static/js/main.4bbbf5d8.chunk.js"
+  },
+  {
+    "revision": "ab5dd6e551ae8216cfa6",
+    "url": "/test2/static/js/runtime-main.293b9844.js"
+  },
+  {
+    "revision": "a82c4c3bfe6cd8a9d57597071772d0c7",
+    "url": "/test2/static/media/Kanit-Regular.a82c4c3b.ttf"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/test2/static/media/Lato-Regular.2d36b1a9.ttf"
+  },
+  {
+    "revision": "c8039f52b7d129caf9343179dd6aff0f",
+    "url": "/test2/static/media/chome_ic.c8039f52.png"
+  },
+  {
+    "revision": "07c43942b1a9179c579e15a1023b3b90",
+    "url": "/test2/static/media/cover01.07c43942.jpg"
+  },
+  {
+    "revision": "b59c979f108229a7f772b3898a790d14",
+    "url": "/test2/static/media/cover02.b59c979f.jpg"
+  },
+  {
+    "revision": "850ad3b27d3293e8ae8eb118d3a4199e",
+    "url": "/test2/static/media/cover11.850ad3b2.jpg"
+  },
+  {
+    "revision": "d19263cedd89557c84aa71a94ab4a9ff",
+    "url": "/test2/static/media/cover11_bottomB.d19263ce.png"
+  },
+  {
+    "revision": "01229ca16126e1a72096f3b2b24052c2",
+    "url": "/test2/static/media/cover11_middle.01229ca1.png"
+  },
+  {
+    "revision": "074630b9ccc767f68b95e438e2fba1bd",
+    "url": "/test2/static/media/cover21.074630b9.png"
+  },
+  {
+    "revision": "c07ed96c9e2257dac8a14f4a39c3376f",
+    "url": "/test2/static/media/cover21_middle.c07ed96c.png"
+  },
+  {
+    "revision": "42d3894377aa21b2000dbce91242b1d2",
+    "url": "/test2/static/media/cover21_top.42d38943.png"
+  },
+  {
+    "revision": "5eb9e49988c6a49b86588718a03b9c5c",
+    "url": "/test2/static/media/forms.5eb9e499.jpg"
+  },
+  {
+    "revision": "4dfdd1c035c641accc4f3fedb7b73055",
+    "url": "/test2/static/media/lato-latin-100.4dfdd1c0.woff"
+  },
+  {
+    "revision": "9ca14b8703044ec44627982d6d0e1e44",
+    "url": "/test2/static/media/lato-latin-100.9ca14b87.woff2"
+  },
+  {
+    "revision": "83a45512c894889710e9966b9edddc6d",
+    "url": "/test2/static/media/lato-latin-100italic.83a45512.woff"
+  },
+  {
+    "revision": "de3428c265cecf1856805f21f274bb82",
+    "url": "/test2/static/media/lato-latin-100italic.de3428c2.woff2"
+  },
+  {
+    "revision": "656963c4beb814e754e38478fcaa0439",
+    "url": "/test2/static/media/lato-latin-300.656963c4.woff"
+  },
+  {
+    "revision": "98d8cf792834c0bef59c2be99dc3533d",
+    "url": "/test2/static/media/lato-latin-300.98d8cf79.woff2"
+  },
+  {
+    "revision": "8e90b967ea69fc68b130d36cc34d41c0",
+    "url": "/test2/static/media/lato-latin-300italic.8e90b967.woff"
+  },
+  {
+    "revision": "a21767e20d27a9c06007c981a8e5f827",
+    "url": "/test2/static/media/lato-latin-300italic.a21767e2.woff2"
+  },
+  {
+    "revision": "b4d2c4c39853ee244272c04999b230ba",
+    "url": "/test2/static/media/lato-latin-400.b4d2c4c3.woff2"
+  },
+  {
+    "revision": "b8ee546acd6cc0c49f42ad3d48ef244f",
+    "url": "/test2/static/media/lato-latin-400.b8ee546a.woff"
+  },
+  {
+    "revision": "117e995c97eab30fb92843616018d1f1",
+    "url": "/test2/static/media/lato-latin-400italic.117e995c.woff2"
+  },
+  {
+    "revision": "e652470b9572b86907b77a97f40dc915",
+    "url": "/test2/static/media/lato-latin-400italic.e652470b.woff"
+  },
+  {
+    "revision": "1efbd38aa76ddae2580fedf378276333",
+    "url": "/test2/static/media/lato-latin-700.1efbd38a.woff2"
+  },
+  {
+    "revision": "874b8e7bc7e8d1507b50f56bc6c9b536",
+    "url": "/test2/static/media/lato-latin-700.874b8e7b.woff"
+  },
+  {
+    "revision": "5ce0990b59e1ed40e05327f625a24c4a",
+    "url": "/test2/static/media/lato-latin-700italic.5ce0990b.woff2"
+  },
+  {
+    "revision": "9e9a574493d4cd2fa62380f376461941",
+    "url": "/test2/static/media/lato-latin-700italic.9e9a5744.woff"
+  },
+  {
+    "revision": "947e87c53b5765bfc8982613ccd789e9",
+    "url": "/test2/static/media/lato-latin-900.947e87c5.woff2"
+  },
+  {
+    "revision": "a82ff6ac9208656f9a21a65dfacbcae3",
+    "url": "/test2/static/media/lato-latin-900.a82ff6ac.woff"
+  },
+  {
+    "revision": "73d7130e1dc196d25bb35faf2afbbb43",
+    "url": "/test2/static/media/lato-latin-900italic.73d7130e.woff"
+  },
+  {
+    "revision": "bd86aaa72d23c91de85390a93aa6bef2",
+    "url": "/test2/static/media/lato-latin-900italic.bd86aaa7.woff2"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test2/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "520fbd02757de4f167f956199ba6d874",
+    "url": "/test2/static/media/print_the_cover.520fbd02.gif"
+  },
+  {
+    "revision": "162633eed2206b6063fb9f672b76c0e8",
+    "url": "/test2/static/media/setup_1.162633ee.png"
+  }
+]);
